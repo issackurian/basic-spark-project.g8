75f9fa41dbe15a78a369940c1c214df07cbb5db1
Issac Kurian <issackurian1@gmail.com>
2017-02-28 11:58:04 -0500

    Create Main.scala

---
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -0,0 +1,13 @@
+package $package$
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+
+
+object Main {
+  def main(args: Array[String]) {
+  }
+}
+
